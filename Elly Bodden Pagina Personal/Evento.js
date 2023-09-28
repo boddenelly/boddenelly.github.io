28eb968ac1c9d7af6ff8b94036155fd3292f6d84
boddenelly <146300129+boddenelly@users.noreply.github.com>
2023-09-27 19:05:44 -0600

    Delete Elly Bodden Pagina Personal directory

--- Elly Bodden Pagina Personal/Evento.js
+++
@@ -1,9 +0,0 @@
-function creaCaja(){
-var contendor = document.getElementById('contenedor');
-var caja = document.createElement('div');
-caja.className = 'caja';
-contendor.appendChild(caja);
-}
-
-var btncaja = document.getElementById('btn-caja');
-btncaja.addEventListener('click', crearCaja);
--- Elly Bodden Pagina Personal/funcion4.js
+++
@@ -1,8 +0,0 @@
-var edad = prompt("Introduce tu edad");
-var Number = 18;
-
-if (Number == edad) {
-  alert("Puedes Conducir");
-} else {
-  alert("No puedes conducir")
-}
